5ccc1690ee8a42cad8823e858f021228e6f90235
Michael Stephens <mstephens.dev@gmail.com>
2020-09-08 11:23:49 -0400

    added parenting toggle to drag demo scene

---
+++ Assets/Scripts/Scenes/2_DragComponents/DragElementParenter.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using GourdUI;
+using TMPro;
+using UnityEngine;
+
+public class DragElementParenter : MonoBehaviour
+{
+    public RectDragSource[] innerDraggers;
+    public Transform dragParent;
+    public TMP_Text buttonText;
+
+    private bool _parented = true;
+
+    public void ToggleParenting()
+    {
+        _parented = !_parented;
+        if (_parented)
+        {
+            foreach (var d in innerDraggers)
+            {
+                d.transform.SetParent(dragParent);
+            }
+        }
+        else
+        {
+            foreach (var d in innerDraggers)
+            {
+                d.transform.SetParent(dragParent.parent);
+            }
+        }
+
+        buttonText.text = _parented ? 
+            "Toggle Elements Parenting (ON)" : 
+            "Toggle Elements Parenting (OFF)";
+    }
+}
